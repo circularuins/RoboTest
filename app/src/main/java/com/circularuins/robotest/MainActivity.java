3dc7c0aa81e6cef8db7baeef2990789467f7bb4a
circularuins <natsuhiko.wake@gmail.com>
2017-12-14 17:20:29 +0900

    画面遷移パターンを追加

--- app/src/main/java/com/circularuins/robotest/MainActivity.java
+++ app/src/main/java/com/circularuins/robotest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.circularuins.robotest;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 bind.tvHoge.setText("fuga");
+            }
+        });
+
+        bind.btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NextActivity.class));
             }
         });
     }
--- app/src/test/java/com/circularuins/robotest/MainActivityTest.java
+++ app/src/test/java/com/circularuins/robotest/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.circularuins.robotest;
 
+import android.content.Intent;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -8,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowApplication;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -20,12 +22,14 @@ public class MainActivityTest {
     private MainActivity activity;
     private TextView tvHoge;
     private Button btnOk;
+    private Button btnNext;
 
     @Before
     public void setUp() throws Exception {
         activity = Robolectric.setupActivity(MainActivity.class);
         tvHoge = (TextView)activity.findViewById(R.id.tv_hoge);
         btnOk = (Button)activity.findViewById(R.id.btn_ok);
+        btnNext = (Button)activity.findViewById(R.id.btn_next);
     }
 
     @Test
@@ -37,5 +41,13 @@ public class MainActivityTest {
     public void clickOk() {
         btnOk.performClick();
         assertEquals("fuga", tvHoge.getText().toString());
+    }
+
+    @Test
+    public void clickNext() {
+        btnNext.performClick();
+        Intent expectedIntent = new Intent(activity, NextActivity.class);
+        Intent actual = ShadowApplication.getInstance().getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
     }
 }
